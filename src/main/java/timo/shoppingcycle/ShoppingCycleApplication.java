0d861338f433edebf36fd2309022be9c4b55d45b
Timo <developer.timo@gmail.com>
2021-12-19 16:59:36 +0900

    Initial commit

---
+++ src/main/java/timo/shoppingcycle/ShoppingCycleApplication.java
@@ -0,0 +1,13 @@
+package timo.shoppingcycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCycleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCycleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/timo/shoppingcycle/ShoppingCycleApplicationTests.java
@@ -0,0 +1,13 @@
+package timo.shoppingcycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCycleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
