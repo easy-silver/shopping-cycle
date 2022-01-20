c450f8d87c00f3102b8757dfc433396ef762dea0
Timo <developer.timo@gmail.com>
2022-01-20 15:12:50 +0900

    Refactor: 마지막 구매건의 용량에 따른 사용일 수 계산 메서드 분리

--- src/main/java/timo/shoppingcycle/domain/Stuff.java
+++ src/main/java/timo/shoppingcycle/domain/Stuff.java
@@ -31,16 +31,20 @@ public class Stuff {
         //1일당 사용량 계산
         double useAmountPerDay = measuringCalculator.calculateUsedPerDay(histories);
 
+        //마지막 구매 이력
         PurchaseHistory lastPurchase = histories.get(histories.size() - 1);
-        LocalDate lastPurchaseDate = lastPurchase.getPurchaseDate();
-        double lastPurchaseCapacity = lastPurchase.getCapacity();
 
-        //마지막 구매건 용량을 하루당 사용량으로 나눈다.
-        int countOfDay = (int) Math.floor(lastPurchaseCapacity / useAmountPerDay);
-        LocalDate nextDate = lastPurchaseDate.plusDays(countOfDay);
+        int countOfDay = calculateCountOfDay(useAmountPerDay, lastPurchase.getCapacity());
+
+        LocalDate nextDate = lastPurchase.getPurchaseDate().plusDays(countOfDay);
 
         //계산된 구매 예정일이 오늘보다 작으면 오늘로 표현
         return nextDate.isBefore(LocalDate.now()) ? LocalDate.now() : nextDate;
+    }
+
+    /** 하루 평균 사용량과 마지막 구매량으로 전부 사용되기까지 소요되는 일 수 계산 */
+    private int calculateCountOfDay(double useAmountPerDay, double lastPurchaseCapacity) {
+        return (int) Math.floor(lastPurchaseCapacity / useAmountPerDay);
     }
 
 }
